edccd31996aff183ffd2b08c0df63eb6233cdb93
dam <damianrolewski@gmail.com>
2019-11-04 09:36:14 +0100

    utworzono projekt z zaleznosciami

---
+++ src/main/java/com/example/demo/IPSApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPSApplication.class, args);
+	}
+
+}
